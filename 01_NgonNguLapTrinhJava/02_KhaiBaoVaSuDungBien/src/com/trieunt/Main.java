b029f6c34d91b23e8c81a127ff361fd48ccdf78d
trieunt1995 <trieunt.qng@gmail.com>
2019-10-12 14:03:35 +0700

    Variable_Datatypes

---
+++ 01_NgonNguLapTrinhJava/02_KhaiBaoVaSuDungBien/src/com/trieunt/Main.java
@@ -0,0 +1,26 @@
+package com.trieunt;
+
+public class Main {
+    public static void main(String[] args) {
+        byte myByte = 5;
+        short myShort = 20;
+        int myInt = 40;
+        long myLong = 400l;
+        double myDouble = 283.28373;
+        float myFloat = 3.14f;
+        char myChar = 'T';
+        boolean myBool = true;
+        String myString = "Nguyễn Tuấn Triều";
+
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("Int: " + myInt);
+        System.out.println("Long: " + myLong);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Float: " + myFloat);
+        System.out.println("char: " + myChar);
+        System.out.println("Boolean: " + myBool);
+        System.out.println("String: " + myString);
+
+    }
+}
