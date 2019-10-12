3a0259dd18bbe25ef068fe106901a81c16ce0df2
trieunt1995 <trieunt.qng@gmail.com>
2019-10-12 13:54:49 +0700

    Java
    Bài tập 1 - Hiển thị thời gian hiện tại

---
+++ 01_NgonNguLapTrinhJava/01_HienThiNgayGioHeThong/src/com/trieunt/Main.java
@@ -0,0 +1,15 @@
+package com.trieunt;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Lấy thời gian hiện tại
+        Date now = new Date();
+
+        // Hiển thị thời gian
+        System.out.println("Thời gian hiện tại: " + now);
+
+    }
+}
