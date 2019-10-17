bd246faa4511699d2694faf3eacade9e5e9fe9b3
trieunt1995 <trieunt.qng@gmail.com>
2019-10-18 03:06:12 +0700

    Update method add element in array

--- 02_MangVaPhuongThuc/06_ChenPhanTuVaoMang/src/com/trieunt/Main.java
+++ 02_MangVaPhuongThuc/06_ChenPhanTuVaoMang/src/com/trieunt/Main.java
@@ -1,14 +1,23 @@
 package com.trieunt;
 
-import java.util.Date;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số lượng phần tử: ");
+        int total = scanner.nextInt();
 
-
+        int[] myArr = addElememtInArray(total);
+        displayAllElementInArray(myArr);
+        System.out.print("\nNhập vị trí cần thêm: ");
+        int position = scanner.nextInt();
+        System.out.print("Nhập giá trị phần tử cần thêm: ");
+        int element = scanner.nextInt();
+        System.out.print("Mảng sau khi thêm là: ");
+        displayAllElementInArray(addNewAElement(position, myArr, element));
 
     }
 
@@ -34,9 +43,14 @@ public class Main {
         }
     }
 
-    public static int[] addNewAElement(int position, int[] myArray) {
 
+    public static int[] addNewAElement(int position, int[] myArray, int element) {
+        int[] newArr = Arrays.copyOf(myArray, myArray.length+1);
+        for (int i = newArr.length-2; i >= position; i--) {
+            newArr[i+1] = newArr[i];
 
-        return myArray;
+        }
+        newArr[position] = element;
+        return newArr;
     }
 }
